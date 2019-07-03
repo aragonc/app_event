1a52e5c07920b9eff58db7f97ac7ecb0e2c78bce
Alex Aragón <aragcar@gmail.com>
2019-07-03 11:11:57 -0500

    add table settings

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -6,5 +6,32 @@
 
 @endsection
 @section('content')
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Configuración</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($settings as $item)
+                <tr>
+                    <th scope="row">{{ $item->display_text }}</th>
+                    <td>{{ $item->value }}</td>
+                    <td>
+                        <div class="actions">
+                            <a href="{{ route('category.edit', $item->id) }}" class="btn btn-primary btn-sm">
+                                <i class="fas fa-pencil-alt"></i>
+                            </a>
+                            {!! Form::close() !!}
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
 
+            </tbody>
+        </table>
+    </div>
 @endsection
