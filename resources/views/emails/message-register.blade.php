e02aab779aef4f7ff98475a57f1e55972667d1f2
Alex Aragón <aragcar@gmail.com>
2019-07-31 16:46:28 -0500

    fix

--- resources/views/emails/message-register.blade.php
+++ resources/views/emails/message-register.blade.php
@@ -21,11 +21,11 @@
     <table style="border: none; width: 100%; padding: 2rem;">
         <tr>
             <td style="text-align: center">
-                <p>Por favor haga clic en el siguiente botón:</p>
+                <p>Clic en el siguiente botón:</p>
                 <a target="_blank" href="{{ $brochure }}">
                     <img src="{{ asset('img/btn_pdf.png') }}" width="200px">
                 </a>
-                <p style="font-style: italic">Por1 favor no responder a este correo, en breve nos estaremos comunicando contigo</p>
+                <p style="font-style: italic">Por favor no responder a este correo, en breve nos estaremos comunicando contigo</p>
             </td>
         </tr>
     </table>
