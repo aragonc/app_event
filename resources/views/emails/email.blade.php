7a055b55f85ae0a50ff78a27250c2c02e82c9d6a
Alex Aragón <aragcar@gmail.com>
2019-09-11 00:45:18 -0500

    update editor event

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>@yield('title')</title>
 </head>
-<body class="" style="background-color: #333; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
-<table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #333;">
+<body class="" style="background-color: {{ $background }}; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+<table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: {{ $background }};">
     <tr>
         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
         <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;">
@@ -66,7 +66,7 @@
                         </tr>
                         <tr>
                             <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                Powered by <a href="https://tunqui.pe" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Tunqui.pe</a>.
+                                Distribuido por <a href="https://tunqui.pe" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Tunqui.pe</a>.
                             </td>
                         </tr>
                     </table>
