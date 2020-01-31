0bc262ebe569488cee574602da9f324ccf9f044d
Alex Aragón <aragcar@gmail.com>
2020-01-31 17:17:12 -0500

    fix export xls, add column event title

--- app/Inc/Exports.php
+++ app/Inc/Exports.php
@@ -3,13 +3,41 @@
 
 namespace App\Inc;
 
+use App\Event;
 use App\People;
+use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class Exports implements FromCollection
+class Exports implements FromArray
 {
     public function collection()
     {
-        return People::all();
+
+    }
+
+    /**
+     * @return array
+     */
+    public function array(): array
+    {
+        $peoples = People::all();
+        $list = [];
+        foreach ($peoples as $people){
+
+            $temp = [
+                'code' => $people->id,
+                'name' => $people->name,
+                'event' => $people->event->title,
+                'dni' => $people->dni,
+                'email' => $people->email,
+                'phone' => $people->phone,
+                'country' => $people->country,
+                'authorize' => $people->authorize,
+                'created_at' => $people->created_at
+            ];
+            $list[] = $temp;
+        }
+
+        return $list;
     }
 }
